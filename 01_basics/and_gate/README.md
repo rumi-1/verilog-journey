dd44a2e7e21bf06bbbf4ce9c85ee7b2a59f66810
rumi <zayd.yusufi786@gmail.com>
2026-03-27 16:40:23 +0800

    Add: AND gate module with testbench and waveform

---
+++ 01_basics/and_gate/README.md
@@ -0,0 +1,18 @@
+# AND Gate
+## Concept
+A 2-input AND Gate outputs HIGH only when both inputs are HIGH (1). 
+Models CMOS transmission gate behavior at a logic level.
+
+## Truth Table
+| a | b | o |
+|---|---|---|
+| 0 | 0 | 0 |
+| 0 | 1 | 0 |
+| 1 | 0 | 0 |
+| 1 | 1 | 1 |
+## Waveform
+![Waveform](waveform.png)
+## Files
+- `and_gate.v`    | module
+- `and_gate_tb.v` | testbench
+- `and_gate.vcd`  | .vcd waveform
--- 01_basics/and_gate/and_gate_tb.v
+++ 01_basics/and_gate/and_gate_tb.v
@@ -1,1 +1,29 @@
-'timescale 1ns / 1ps
+`timescale 1ns / 1ps
+
+module and_gate_tb;
+    reg a, b;
+    wire o;
+
+    and_gate uut (
+        .a(a),
+        .b(b),
+        .o(o)
+    );
+
+    // generate waveform file
+    initial begin
+        $dumpfile("and_gate.vcd");
+        $dumpvars(0, and_gate_tb);
+    end
+
+    // terminal monitor
+    initial begin
+        $monitor("Time=%0t | a=%b b=%b | o=%b", $time, a, b, o);
+        a = 0; b = 0; #10; // #10 = delay 10 nanoseconds
+        a = 0; b = 1; #10; 
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+        $finish;
+    end
+
+endmodule
